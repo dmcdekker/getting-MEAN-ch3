29731ff351e6e2df33a792674d22a5c7af927e0f
Denise <ddekker@mills.edu>
2017-02-04 22:10:33 +0000

    Procfile added

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
+
+//require main controllers file
 var ctrlMain = require('../controllers/main');
 
+//reference index method of controllers in route of definition
 /* GET home page. */
 router.get('/', ctrlMain.index);
 
