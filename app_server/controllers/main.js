e773b705035afd3294f6eb2886831b319e474f0d
Denise <ddekker@mills.edu>
2017-02-13 23:58:07 +0000

    last save for ch3

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,5 +1,5 @@
 /* GET home page */
-//create index export method
+//create index export method so can be used outside this doc
 module.exports.index = function (req,res){
     //include controller code for homepage
     res.render('index', {title: 'Express'});
